24e7daadb01837a962bec8e3d66b1cf5c3fdb8e8
Rafael Schwan <rafaelschwan@hotmail.com>
2020-08-11 21:37:08 -0300

    Primeiro commit da aplicação para repositório

---
+++ src/routes/Route.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
+
+import { store } from '~/store';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const { signed } = store.getState().auth;
+
+  // Se usuario nao esta logado e quer acessar rota privada, vai para login
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  // Se usuario esta logado e rota não é privada manda pro dashboard
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
